deb40699e1870480b45550d03a9eda52a6cbaffd
Mike Vink <mike1994vink@gmail.com>
2023-10-09 16:16:05 +0000

    fixup

--- flake.nix
+++ flake.nix
@@ -52,7 +52,6 @@
         ({config, ... }: {
           nixpkgs.overlays = with lib; [(composeManyExtensions [
             (import ./overlays/vimPlugins.nix {inherit pkgs;})
-            (import ./overlays/suckless.nix {inherit pkgs; home = config.users.users.mike.home;})
           ])];
         })
         ./machines/wsl.nix
