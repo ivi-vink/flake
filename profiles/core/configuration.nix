40b0c12e3e7a106c90fa454f03b25efddcef2e67
Mike Vink <mike1994vink@gmail.com>
2023-10-18 23:11:16 +0200

    fixup

--- profiles/core/configuration.nix
+++ profiles/core/configuration.nix
@@ -7,12 +7,18 @@
   imports = [ (mkAliasOptionModule [ "ivi" ] [ "users" "users" ivi.username ]) ];
 
   time.timeZone = "Europe/Amsterdam";
-  users.users.${ivi.username} = {
-    uid = 1000;
-    isNormalUser = true;
-    description = ivi.realName;
-    extraGroups = ["wheel" "networkmanager" "docker" "transmission"];
-    openssh.authorizedKeys.keys = ivi.sshKeys;
+  users.users = {
+      ${ivi.username} = {
+        uid = 1000;
+        isNormalUser = true;
+        description = ivi.realName;
+        extraGroups = ["wheel" "networkmanager" "docker" "transmission"];
+        openssh.authorizedKeys.keys = ivi.sshKeys;
+      };
+      root = {
+        passwordFile = secrets.password.path;
+        openssh.authorizedKeys.keys = config.ivi.openssh.authorizedKeys.keys;
+      };
   };
   security = {
     sudo = {
