51568cf1e1195aa53028c225802da429841904df
Mike Vink <ivi@vinkies.net>
2024-01-11 23:37:11 +0100

    indent

--- profiles/homeserver/radicale.nix
+++ profiles/homeserver/radicale.nix
@@ -1,13 +1,13 @@
 { lib, ... }: with lib; {
   services.nginx = {
-      virtualHosts."cal.${ivi.domain}"  = {
-          locations."/" = {
-            proxyPass = "http://127.0.0.1:5232";
-          };
+    virtualHosts."cal.${ivi.domain}"  = {
+      locations."/" = {
+        proxyPass = "http://127.0.0.1:5232";
       };
+    };
   };
   services.radicale = {
-      enable = true;
-      settings.server.hosts = [ "0.0.0.0:5232" ];
+    enable = true;
+    settings.server.hosts = [ "0.0.0.0:5232" ];
   };
 }
