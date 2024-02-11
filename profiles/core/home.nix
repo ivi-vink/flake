11e4a85134d79c2a9f74f4501ea45181facf38f6
Mike Vink <mike@pionative.com>
2024-02-11 21:22:14 +0100

    new ssh key

--- profiles/core/home.nix
+++ profiles/core/home.nix
@@ -50,7 +50,7 @@
       enable = true;
       matchBlocks = {
         "*" = {
-          identityFile = "${config.ivi.home}/.ssh/id_ed25519";
+          identityFile = "${config.ivi.home}/.ssh/id_ed25519_sk";
         };
       };
     };
