13c3bd8573543493df392bc21630960029c921d2
Mike Vink <mike@pionative.com>
2024-07-18 08:38:51 +0200

    split script for netboot

--- profiles/netboot/system.nix
+++ profiles/netboot/system.nix
@@ -4,11 +4,19 @@ sys: { pkgs, lib, ... }: let
     in pkgs.writeShellApplication {
       name = "run-pixiecore";
       text = ''
-        exec ${pkgs.pixiecore}/bin/pixiecore \
-          boot ${build.kernel}/bzImage ${build.netbootRamdisk}/initrd \
-          --cmdline "init=${build.toplevel}/init loglevel=4" \
+        sudo ${pkgs.pixiecore}/bin/pixiecore \
+          boot kernel/bzImage initrd/initrd \
+          --cmdline "init=init/init loglevel=4" \
           --debug --dhcp-no-bind \
           --port 64172 --status-port 64172 "$@"
+      '';
+    };
+  build-pixie = pkgs.writeShellApplication {
+      name = "build-pixie";
+      text = ''
+        nix build /nix-config\#nixosConfigurations."$1".config.system.build.kernel --impure -o kernel
+        nix build /nix-config\#nixosConfigurations."$1".config.system.build.toplevel --impure -o init
+        nix build /nix-config\#nixosConfigurations."$1".config.system.build.netbootRamdisk --impure -o initrd
       '';
     };
 in {
@@ -16,5 +24,6 @@ in {
   networking.firewall.allowedTCPPorts = [ 64172 ];
   environment.systemPackages = [
     run-pixiecore
+    build-pixie
   ];
 }
