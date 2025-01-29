8eaa94e7af55bc6ceb2ffbd208143efcf392370a
Mike Vink <59492084+ivi-vink@users.noreply.github.com>
2025-01-29 14:24:53 +0000

    fixup

--- mut/vis/visrc.lua
+++ mut/vis/visrc.lua
@@ -1,5 +1,10 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+local format = require('vis-format')
+for k, _ in pairs(format.formatters) do
+	format.formatters[k] = nil
+end
+format.formatters.python = format.stdio_formatter("ruff format -")
 
 vis.events.subscribe(vis.events.INIT, function()
   vis:command"set shell '/usr/bin/bash'"
@@ -8,24 +13,6 @@ end)
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 end)
 
-On branch master
-Your branch is up to date with 'origin/master'.
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   justfile
-	modified:   mut/aerospace/aerospace.toml
-	modified:   mut/bin/pnsh-nvim
-	modified:   mut/bin/xdg-open
-	modified:   mut/neovim/lua/my/init.lua
-	modified:   mut/nushell/config.nu
-
-Untracked files:
-  (use "git add <file>..." to include in what will be committed)
-	mut/carapace/specs/upctl.yaml
-	mut/vis/
-	nohup.out
-
-no changes added to commit (use "git add" and/or "git commit -a")
-
+vis.events.subscribe(vis.events.FILE_SAVE_PRE, function(win)
+  format.apply(win)
+end)
