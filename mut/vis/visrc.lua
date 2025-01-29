e23e491f69cab90c0e6815fc7c5be5c9bb4c5185
Mike Vink <59492084+ivi-vink@users.noreply.github.com>
2025-01-29 14:42:31 +0000

    fixup

--- mut/vis/visrc.lua
+++ mut/vis/visrc.lua
@@ -1,10 +1,13 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+require('vis-editorconfig')
+
 local format = require('vis-format')
 for k, _ in pairs(format.formatters) do
 	format.formatters[k] = nil
 end
 format.formatters.python = format.stdio_formatter("ruff format -", {on_save=true})
+
 
 vis.events.subscribe(vis.events.INIT, function()
   vis:command"set shell '/usr/bin/bash'"
