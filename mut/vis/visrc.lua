3f6e4c0707f411cb8ea3f4c541ec0001153aa6e1
Mike Vink <59492084+ivi-vink@users.noreply.github.com>
2025-01-29 14:34:13 +0000

    fixup

--- mut/vis/visrc.lua
+++ mut/vis/visrc.lua
@@ -4,15 +4,12 @@ local format = require('vis-format')
 for k, _ in pairs(format.formatters) do
 	format.formatters[k] = nil
 end
-format.formatters.python = format.stdio_formatter("ruff format -")
+format.formatters.python = format.stdio_formatter("ruff format -", {on_save=true})
 
 vis.events.subscribe(vis.events.INIT, function()
   vis:command"set shell '/usr/bin/bash'"
+  vis:command"set edconfhooks on"
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-end)
-
-vis.events.subscribe(vis.events.FILE_SAVE_PRE, function(win)
-  format.apply(win)
 end)
